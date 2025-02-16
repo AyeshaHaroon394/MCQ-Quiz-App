1aaec87d49d9a6b648124566b19ef1be9208b6fd
AyeshaHaroon394 <ayeshaharoon394@gmail.com>
2025-02-16 20:59:13 +0500

    Start activity backend added ie MainActivity.java

--- app/src/main/java/com/example/my_quiz_app/MainActivity.java
+++ app/src/main/java/com/example/my_quiz_app/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.my_quiz_app;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
     EditText nameField;
 
 
+    //Initializing the components
     public void init()
     {
         logo = findViewById(R.id.idlogo);
@@ -28,10 +32,31 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        init();
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        //Making sure field is not left blank by using Toasting and getting the username from the field
+        startbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username=nameField.getText().toString();
+
+                if(username.isEmpty())
+                {
+                    Toast.makeText(MainActivity.this, "Can not leave Name Field blank!", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    Intent i = new Intent(MainActivity.this, QuizScreen.class);
+                    i.putExtra("USERNAME", username);
+                    startActivity(i);
+                    finish();
+                }
+            }
         });
     }
 }
