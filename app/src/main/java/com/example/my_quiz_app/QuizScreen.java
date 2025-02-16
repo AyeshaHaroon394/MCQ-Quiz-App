b158a5b60e3099009c1f2e4a232ddae43105b80f
AyeshaHaroon394 <ayeshaharoon394@gmail.com>
2025-02-16 20:47:39 +0500

    Result screen frontend and partial backend

--- app/src/main/java/com/example/my_quiz_app/QuizScreen.java
+++ app/src/main/java/com/example/my_quiz_app/QuizScreen.java
@@ -10,6 +10,32 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class QuizScreen extends AppCompatActivity {
 
+    private String[] questions = {
+            "In Harry Potter and the Philosopher's Stone, what object does Harry first catch in his mouth during a Quidditch match?",
+            "What is the name of the Weasley family’s home?",
+            "What is the name of the house-elf who serves the Malfoy family?",
+            "Which of these spells is used to disarm an opponent?",
+            "What type of dragon does Harry face in the Triwizard Tournament?",
+            "Who is the ghost of Ravenclaw house?",
+            "What is the name of the train that takes students to Hogwarts?",
+            "What does Dumbledore leave to Hermione in his will?",
+            "Which magical creature pulls the carriages at Hogwarts?",
+            "What does the Marauder’s Map reveal?"
+    };
+
+    private String[][] options = {
+            {"Remembrall", "Snitch", "Bludger", "Quaffle"},
+            {"The Nest", "The Burrow", "Ottery St. Catchpole", "Hogsmeade Cottage"},
+            {"Dobby", "Winky", "Kreacher", "Hokey"},
+            {"Expelliarmus", "Avada Kedavra", "Alohomora", "Crucio"},
+            {"Hungarian Horntail", "Norwegian Ridgeback", "Swedish Short-Snout", "Common Welsh Green"},
+            {"The Grey Lady", "The Bloody Baron", "Nearly Headless Nick", "The Fat Friar"},
+            {"Knight Bus", "Hogwarts Express", "The Lightning Train", "Wizarding Locomotive"},
+            {"A wand", "The Tales of Beedle the Bard", "A Deluminator", "A Snitch"},
+            {"Hippogriffs", "Thestrals", "Dragons", "Basilisks"},
+            {"Hidden messages", "People’s thoughts", "Secret passages", "The location of everyone in Hogwarts"}
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- app/src/main/java/com/example/my_quiz_app/ResultScreen.java
+++ app/src/main/java/com/example/my_quiz_app/ResultScreen.java
@@ -1,9 +1,6 @@
 package com.example.my_quiz_app;
 
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,23 +8,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-    ImageView logo;
-    Button startbtn;
-    EditText nameField;
+public class ResultScreen extends AppCompatActivity {
 
-
-    public void init()
-    {
-        logo = findViewById(R.id.idlogo);
-        startbtn = findViewById(R.id.startbtn);
-        nameField = findViewById(R.id.username);
-    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.result_screen);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
